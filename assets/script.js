b87ac78abac7a1fba48e5c48e9e1910f318daa34
melissikiss <melissikiss@gmail.com>
2020-10-09 12:45:12 -0700

    added objects

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,46 @@
-var startButton = document.querySelector("#start-button");
-var buttonChoice = document.createElement("button");
-var testArea = document.querySelector("#options");
+var quizQuestions = [
+{ question: 'Inside which HTML element do we put the Javascript?', 
+    choice1: "<script>", 
+    choice2: "<js>",
+    choice3: "<scripting>",
+    choice4: "<javascript>",
+    answer: "<script>" },
 
-var beginQuiz = function(event) {
-    buttonChoice.textContent = "Option-1";
-    buttonChoice.className = "button";
-    testArea.appendChild(buttonChoice);
+{ question: 'Where is the correct place to insert a Javascript?',
+    choice1: "Both the <head> section and the <body> section are correct",
+    choice2: "The <head> section",
+    choice3: "The bottom of the <body> section", 
+    choice4: "The very top of the <body> section",
+    answer: "The bottom of <body> section" },
+
+{ question: 'Which built-in method returns the length of the string?',
+    choice1: "size()",
+    choice2: "length()",
+    choice3: "long()", 
+    choice4: "All of the above",
+    answer: "length()" },
+
+{ question: 'How do you write "Hello World" in an alert box?', 
+    choice1: "('Hello World');",
+    choice2: "response.write('Hello World');",
+    choice3: "alert('Hello World');",
+    choice4: "msg('Hello World');",
+    answer: "alert('Hello World');" }, 
+
+{ question: 'How do you create a function in Javascript?',
+    choice1: "function: myFunction()",
+    choice2: "function myFunction()",
+    choice3: "function - myFunction()",
+    choice4: "function = myFunction()",
+    answer: "function = myFunction()", },
+];
+
+var countdownBegin = document.getElementById('start');
+var score = 0;
+
+function startQuiz() {
+
+    for (var i = 0; i < quizQuestions.length; i++) {
+
+    }
 }
-
-startButton.addEventListener("click", beginQuiz);

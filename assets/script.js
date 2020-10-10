4443978babf18330de7b9d1827af04853cab5e11
melissikiss <melissikiss@gmail.com>
2020-10-09 20:26:14 -0700

    new commit

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,62 @@
 var quizQuestions = [
 { question: 'Inside which HTML element do we put the Javascript?', 
-    choice1: "<script>", 
-    choice2: "<js>",
-    choice3: "<scripting>",
-    choice4: "<javascript>",
+    choices: ["<script>", "<js>", "scripting>", "<javascript>"],
     answer: "<script>" },
 
 { question: 'Where is the correct place to insert a Javascript?',
-    choice1: "Both the <head> section and the <body> section are correct",
-    choice2: "The <head> section",
-    choice3: "The bottom of the <body> section", 
-    choice4: "The very top of the <body> section",
+    choices: ["Both the <head> section and the <body> section are correct", "The <head> section", "The bottom of the <body> section", "The very top of the <body> section"],
     answer: "The bottom of <body> section" },
 
 { question: 'Which built-in method returns the length of the string?',
-    choice1: "size()",
-    choice2: "length()",
-    choice3: "long()", 
-    choice4: "All of the above",
+    choices: ["size()", "length()", "long()", "All of the above"],
     answer: "length()" },
 
 { question: 'How do you write "Hello World" in an alert box?', 
-    choice1: "('Hello World');",
-    choice2: "response.write('Hello World');",
-    choice3: "alert('Hello World');",
-    choice4: "msg('Hello World');",
+    choices: ["('Hello World');", "response.write('Hello World');", "alert('Hello World');", "msg('Hello World');"],
     answer: "alert('Hello World');" }, 
 
 { question: 'How do you create a function in Javascript?',
-    choice1: "function: myFunction()",
-    choice2: "function myFunction()",
-    choice3: "function - myFunction()",
-    choice4: "function = myFunction()",
+    choices: ["function: myFunction()", "function myFunction()", "function - myFunction()", "function = myFunction()"], 
     answer: "function = myFunction()", },
 ];
 
 var countdownBegin = document.getElementById('start');
 var seconds = document.getElementById('timer-text');
 var count = 60;
+var currentQuestion = quizQuestions[0];
+var questionContainer = document.getElementById('question-container');
+var questionHeader = document.getElementById('question-header');
+var answers = document.getElementsByClassName('answer');
 
-function startQuiz() {
-    for (var i = 0; i < quizQuestions.length; i++) {
+console.log({questionHeader});
 
+function displayQuestion() {
+    questionHeader.textContent = currentQuestion.question;
+    for ( i = 0; i < answers.length; i++) {
+        answers[i].textContent = currentQuestion.choices[i];
+        answers[i].addEventListener("click", checkAnswer);
     }
+
 }
+
+function checkAnswer(event) {
+    var chosenAnswer = event.target.textContent;
+    console.log({chosenAnswer});
+    if (chosenAnswer = currentQuestion.answer) {
+        console.log("hello!");
+    }
+    else {
+        console.log("hello");
+    }
+    displayQuestion();
+}
+
 
 countdownBegin.addEventListener("click", function() {
     setInterval(function () {
-    count-- ;
-    seconds.textContent = "Time left: " + count;
-}, 1000);
+        count-- ;
+        seconds.textContent = "Time left: " + count;
+    }, 1000);
+    displayQuestion();
 });
+
